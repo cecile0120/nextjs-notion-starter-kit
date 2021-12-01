a2ca36928d27842684e0603fbc4d51cbbc7f5f3e
cecile0120 <95366337+cecile0120@users.noreply.github.com>
2021-12-01 23:29:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'c125f1da5d644f66b63c5364d802607b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '看見',
+  domain: 'https://crocus-marquess-b57.notion.site/website-c125f1da5d644f66b63c5364d802607b',
+  author: '隙隙',
 
   // open graph metadata (optional)
   description: 'Example site description',
